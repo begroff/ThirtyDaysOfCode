9de59d5e2624fd550ec7ef06f3a7d5df0932bcb5
groffTECH <brett@grofftech.net>
2018-09-16 17:17:22 -0400

    Add code for 30 days of code inheritance challenge

---
+++ Sandbox/ThirtyDaysOfCode/Person.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Linq;
+
+public class Person {
+	protected string firstName;
+	protected string lastName;
+	protected int id;
+
+	public Person()
+    {
+
+    }
+
+	public Person(string firstName, string lastName, int identification)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = identification;
+	}
+
+	public void printPerson(){
+		Console.WriteLine("Name: " + lastName + ", " + firstName + "\nID: " + id);
+	}
+}
---
+++ SandboxTests/ThirtyDaysOfCode/PersonStudent.Tests.cs
@@ -0,0 +1,42 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sandbox.ThirtyDaysOfCode;
+using System.IO;
+using System;
+
+namespace SandboxTests
+{
+    [TestClass]
+    public class PersonStudentTests
+    {
+        private string firstName = "";
+        private string lastName = "";
+        private int id = 0;
+        private int[] scores;
+        private char letterGrade;
+
+        [TestMethod]
+        public void CalculateAverageGrade_OutputIsCorrect()
+        {
+            firstName = "Brett";
+            lastName = "Groff";
+            id = 69211269;
+            scores = new int[3] { 100, 90, 75 };
+            Student newStudent = new Student(firstName, lastName, id, scores);
+            string expectedOutput = "";
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+
+                newStudent.printPerson();
+                letterGrade = newStudent.Calculate();
+                newStudent.printLetterGrade();
+
+                expectedOutput = $"Name: {lastName}, {firstName}\nID: {id}\nGrade: {letterGrade}";
+
+                Assert.AreEqual('E', letterGrade);
+                Assert.AreEqual(expectedOutput, sw.ToString());
+            }
+        }
+    }
+}
